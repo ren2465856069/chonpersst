3b08fe6e6783ee93d49676e696a8af3af0ae9da5
任涛 <15573005196@163.com>
2018-07-03 18:04:38 +0800

    xiaotao

---
+++ rentao/js/index.js
@@ -0,0 +1,38 @@
+//  网站首页JS
+
+$(function() {
+
+
+    // 首页轮播
+//  var mySwiper = new Swiper ('.swiper-container', {
+//    direction: 'horizontal',
+//    loop: true,
+//    autoplay: true,
+//    // 如果需要前进后退按钮
+//    navigation: {
+//      nextEl: '.swiper-button-next',
+//      prevEl: '.swiper-button-prev',
+//    }
+//    
+//  })        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
